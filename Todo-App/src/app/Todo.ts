f2625a17ff0a4650bdac3fbfcad261f54483746f
Aditya <f20220497@goa.bits-pilani.ac.in>
2024-06-02 21:53:16 +0530

    added todos component

---
+++ Todo-App/src/app/Todo.ts
@@ -0,0 +1,6 @@
+export class Todo{
+    srno : number | undefined
+    title : string | undefined
+    desc : string | undefined
+    active : boolean | undefined
+} 
--- Todo-App/src/app/mycomponents/todos/todos.component.ts
+++ Todo-App/src/app/mycomponents/todos/todos.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
-
+import { Todo } from 'src/app/Todo';
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent {
-
+  todos: Todo[] | undefined;
+  constructor(){
+    this.todos = [
+      {
+        srno : 1,
+        title : 'title1 here',
+        desc : 'desc1 here',
+        active : true
+      },
+      {
+        srno : 2,
+        title : 'title2 here',
+        desc : 'desc2 here',
+        active : true
+      },
+      {
+        srno : 3,
+        title : 'title3 here',
+        desc : 'desc3 here',
+        active : true
+      },
+      {
+        srno : 4,
+        title : 'title4 here',
+        desc : 'desc4 here',
+        active : true
+      }
+    ]
+  }
 }
